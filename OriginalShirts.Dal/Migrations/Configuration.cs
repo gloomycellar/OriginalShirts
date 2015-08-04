4c6bdc111ee484a465b1352d8a69a8612fa7d8c8
gloomycellar <gloomycellar@gmail.com>
2015-08-04 13:48:21 +0300

    models updated; colors, tags, sizes db initialization added

--- OriginalShirts.Dal/Migrations/Configuration.cs
+++ OriginalShirts.Dal/Migrations/Configuration.cs
@@ -1,31 +1,87 @@
 namespace OriginalShirts.Dal.Migrations
 {
-    using System;
-    using System.Data.Entity;
+    using Models;
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<OriginalShirts.Dal.ApplicationContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<ApplicationContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
         }
 
-        protected override void Seed(OriginalShirts.Dal.ApplicationContext context)
+        protected override void Seed(ApplicationContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            //XS, S, M, L, XL, XXL
+            context.Set<Size>().Add(new Size("XS"));
+            context.Set<Size>().Add(new Size("S"));
+            context.Set<Size>().Add(new Size("M"));
+            context.Set<Size>().Add(new Size("L"));
+            context.Set<Size>().Add(new Size("XL"));
+            context.Set<Size>().Add(new Size("XXL"));
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            //White, Black, Red, Blue, Gray, Green, Pink
+            context.Set<Color>().Add(new Color("White"));
+            context.Set<Color>().Add(new Color("Black"));
+            context.Set<Color>().Add(new Color("Red"));
+            context.Set<Color>().Add(new Color("Blue"));
+            context.Set<Color>().Add(new Color("Gray"));
+            context.Set<Color>().Add(new Color("Green"));
+            context.Set<Color>().Add(new Color("Pink"));
+
+            context.Set<Tag>().Add(new Tag("Nike"));
+            context.Set<Tag>().Add(new Tag("Under Armour"));
+            context.Set<Tag>().Add(new Tag("Adidas"));
+            context.Set<Tag>().Add(new Tag("Puma"));
+            context.Set<Tag>().Add(new Tag("Asics"));
+            context.Set<Tag>().Add(new Tag("Sportwear"));
+            context.Set<Tag>().Add(new Tag("Fendi"));
+            context.Set<Tag>().Add(new Tag("Guess"));
+            context.Set<Tag>().Add(new Tag("Valentino"));
+            context.Set<Tag>().Add(new Tag("Dior"));
+            context.Set<Tag>().Add(new Tag("Versache"));
+            context.Set<Tag>().Add(new Tag("Armani"));
+            context.Set<Tag>().Add(new Tag("Prada"));
+            context.Set<Tag>().Add(new Tag("Dolce And Gabbana"));
+            context.Set<Tag>().Add(new Tag("Chanel"));
+            context.Set<Tag>().Add(new Tag("Gucci"));
+            context.Set<Tag>().Add(new Tag("Mens"));
+            context.Set<Tag>().Add(new Tag("Womens"));
+
+            //List<Tag> children = new List<Tag>()
+            //{
+            //    context.Set<Tag>().Where(x => x.Name == "Sportwear").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Nike").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Under Armour").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Adidas").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Puma").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Asics").First()
+            //};
+
+            //children = new List<Tag>()
+            //{
+            //    context.Set<Tag>().Where(x => x.Name == "Mens").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Fendi").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Guess").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Valentino").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Dior").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Versache").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Armani").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Prada").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Dolce And Gabbana").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Chanel").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Gucci").First()
+            //};
+
+            //children = new List<Tag>()
+            //{
+            //    context.Set<Tag>().Where(x => x.Name == "Womens").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Fendi").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Guess").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Valentino").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Dior").First(),
+            //    context.Set<Tag>().Where(x => x.Name == "Versache").First()
+            //};
         }
     }
 }
--- OriginalShirts.Dal/Models/EntityBase.cs
+++ OriginalShirts.Dal/Models/EntityBase.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace OriginalShirts.Dal.Models
 {
     public class EntityBase
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
     }
 }
